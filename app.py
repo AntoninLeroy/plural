cafebfb1512705953b2fd8583888b322f1feb2fd
Antonin Leroy <leroyantonin.pro@gmail.com>
2022-03-29 15:22:45 +0200

    forgot to waitress

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from waitress import serve
 app = create_app()
 
 if __name__ == '__main__':
-    #serve(app, host='0.0.0.0', port=8000)
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    serve(app, host='0.0.0.0', port=8000)
+    #app.run(host='0.0.0.0', port=8000, debug=True)
